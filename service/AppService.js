d6dde6b66da773693b1a1db3c374c96bd84f107e
nhanhas <mpereira132@hotmail.com>
2019-02-26 22:32:48 +0000

    S#01 - Very First commit

---
+++ service/AppService.js
@@ -0,0 +1,11 @@
+/**
+ * This Service will hold all the needed Requests
+ * and some manipulations to return to Frontend
+ */
+app.service('AppService', ['$http', 'FrameworkUtils', function($http, FrameworkUtils) {
+
+    //App Service URL  
+    //this.serviceWS = '';
+    
+    
+}]);
