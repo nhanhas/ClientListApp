0d2f6ec471bc9e3ac332593dd4bb22d53249652e
nhanhas <mpereira132@hotmail.com>
2019-02-27 21:59:59 +0000

    S#03 - Client List

--- service/AppService.js
+++ service/AppService.js
@@ -5,7 +5,96 @@
 app.service('AppService', ['$http', 'FrameworkUtils', function($http, FrameworkUtils) {
 
     //App Service URL  
-    //this.serviceWS = '';
-    
+    this.serviceWS = 'http://localhost';
+
+    /********************************************************************************
+     * Home
+     ********************************************************************************/
+    /**
+     * #A - Get Client List to Home
+     * parameter to server: none. 
+     * It is a GET 
+     */
+    this.HOME_getAllClients  = function(){
+        let clientsList = [
+            {
+                id : 1,
+                name : 'Miguel Pereira',
+                email : 'mp@ht.com',
+                address : 'Lisboa',
+                phone : '912231234'
+            },
+            {
+                id : 2,
+                name : 'Jõao Batalha',
+                email : 'jb@ht.com',
+                address : 'Oeiras',
+                phone : '914443234'
+            },
+            {
+                id : 3,
+                name : 'Gilberta Dias',
+                email : 'gd@ht.com',
+                address : 'Azeitão',
+                phone : '914422234'
+            },
+            {
+                id : 4,
+                name : 'Rui Pereira',
+                email : '32@ht.com',
+                address : 'Lisboa',
+                phone : '912231234'
+            },
+            {
+                id : 5,
+                name : 'Miguel Mota',
+                email : 'dd@ht.com',
+                address : 'Oeiras',
+                phone : '914443234'
+            },
+            {
+                id : 6,
+                name : 'Lopes Gonga',
+                email : 'sad@ht.com',
+                address : 'Azeitão',
+                phone : '914422234'
+            },
+            {
+                id : 7,
+                name : 'Quasimodo Soares',
+                email : 'mgap@ht.com',
+                address : 'Lisboa',
+                phone : '912231234'
+            },
+            {
+                id : 8,
+                name : 'Denise Teixeira',
+                email : 'jbhsd@ht.com',
+                address : 'Oeiras',
+                phone : '914443234'
+            },
+            {
+                id : 9,
+                name : 'Pereira da Cunha',
+                email : 'gsdd@ht.com',
+                address : 'Azeitão',
+                phone : '914422234'
+            },
+        ];
+
+        return new Promise(function(resolve, reject) {
+            setTimeout(function() {
+                resolve( {data : clientsList} );
+            });
+        });
+
+        //Use the code below
+
+        let serviceURL = this.serviceWS + '/getAllClients';
+
+        return FrameworkUtils.Http_GET(serviceURL).then(function(result){     
+            return result.data;
+        });
+    }    
     
 }]);
--- views/home/HomeController.js
+++ views/home/HomeController.js
@@ -1,9 +1,76 @@
 app
-.controller('HomeController', ['$scope', '$location', 'AppService', function($scope, $location, AppService) { 
+.controller('HomeController', ['$scope', '$location','$q', 'AppService', function($scope, $location, $q , AppService) { 
 	
+	/**
+	 * Controller variables
+	 */
 	$scope.view = {
-		searchExpression : ''
+		isLoading : true,
+		searchExpression : '',
+		clientList : []
 	};
+
+
+
+	//#INITIALIZE Home data
+	$scope.initialize = function(){
+		
+		//#1 - Prepare Queue of promises
+		let promises = [];
+		
+		//#1.1 - Promotions
+		promises.push($scope.getAllClients());
+
+		//#Finally get all promises
+		$q.all(promises).then((result)=>{
+			//Show screen			
+			$scope.view.isLoading = false;
+			
+			
+		});
+
+	};
+
+
+	/**
+	 * Functions to retrieve 
+	 * server data
+	 */
+	//#A - Get Clients from server
+	$scope.getAllClients = function(){
+
+		//#1 - Get Clients by calling server
+		return AppService.HOME_getAllClients().then((result)=>{
+
+			console.log(result);
+
+			//#2 - Process result
+			if(result && result.data){
+				let listOfClients = result.data;
+								
+				//#3 - Iterate list to prepare to show
+				listOfClients.forEach((client)=>{
+
+					//#3.1 - Create a new item 
+					let newClient = {
+						id: client.id,
+						name: client.name,
+						email: client.email,
+						phone : client.phone,
+						address : client.address
+					};
+
+					//#3.2 - Push it to client List
+					$scope.view.clientList.push(newClient);
+				});
+
+			}
+
+		});
+	};
+
+	//#1 - Load Application Data from Server
+	$scope.initialize();
 
 }]);
 
